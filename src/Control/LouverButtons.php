adbb8a0fc14abd9c0263f7afcbf43631978900f2
Paul Water <p.r.water@setbased.nl>
2021-01-17 09:45:21 +0100

    Use correct RenderWalker.

--- src/Control/LouverButtons.php
+++ src/Control/LouverButtons.php
@@ -19,23 +19,14 @@ class LouverButtons extends ComplexControl
    */
   private int $colspan = 1;
 
-  /**
-   * The CSS module class for the footer elements.
-   *
-   * @var string
-   */
-  private string $moduleClass;
-
   //--------------------------------------------------------------------------------------------------------------------
   /**
    * @inheritdoc
    */
   public function getHtml(RenderWalker $walker): string
   {
-    $myWalker = new RenderWalker($this->moduleClass, $this->moduleClass);
-
-    $buttonAttributes  = ['class' => $myWalker->getClasses('button')];
-    $buttonAttributes2 = ['class' => $myWalker->getClasses('button'), 'colspan' => $this->colspan];
+    $buttonAttributes  = ['class' => $walker->getClasses('button')];
+    $buttonAttributes2 = ['class' => $walker->getClasses('button'), 'colspan' => $this->colspan];
 
     $ret = Html::generateTag('tfoot', $buttonAttributes);
     $ret .= Html::generateTag('tr', $buttonAttributes);
@@ -61,21 +52,6 @@ class LouverButtons extends ComplexControl
   public function setColspan(int $colspan): self
   {
     $this->colspan = $colspan;
-
-    return $this;
-  }
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * sets the CSS module class for the footer elements.
-   *
-   * @param string $moduleClass The CSS module class for the footer elements.
-   *
-   * @return LouverButtons
-   */
-  public function setModuleClass(string $moduleClass): LouverButtons
-  {
-    $this->moduleClass = $moduleClass;
 
     return $this;
   }
